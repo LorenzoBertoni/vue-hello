b656b7b2dacff45e8ccef169bc2a2b34d34489bb
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-07-21 13:34:52 +0200

    add: stampato nell'h1 del DOM un messaggio conservato dentro la variabile per VUE + immagine anch'essa conservata nella variabile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 const myVue = new Vue({
     el: '#root',
+    data: {
+        message: 'Benvenuto in...',
+        image: 'https://cdn.iconscout.com/icon/free/png-256/vuejs-1175052.png'
+    }
 })
